6dbf51d7c515dea71f9084c1f2c9bb4396006e27
iydachbuawel-droid <iydach.buawel@gmail.com>
2026-08-06 19:32:56 +0800

    Add files via upload

--- v3-features.js
+++ v3-features.js
@@ -4,7 +4,7 @@
 (() => {
   'use strict';
 
-  const APP_VERSION = '3.0.0';
+  const APP_VERSION = '3.0.1';
   const BACKUP_DB = 'shirogane-auto-backups';
   const BACKUP_STORE = 'backups';
   const MAX_BACKUPS = 10;
